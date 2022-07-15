ae80f3257c5b7e9104744fd670dd8e960e0155e6
jklq <sebastian@angeltvedt.net>
2022-07-15 14:17:06 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/jklq/go-feature-flag-solidjs-demo/v2
+
+go 1.18
+
+require (
+	github.com/gofiber/fiber/v2 v2.35.0
+	github.com/google/uuid v1.3.0
+	github.com/thomaspoignant/go-feature-flag v0.26.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220626175859-9abda183db8e // indirect
+	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/nikunjy/rules v1.1.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.38.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	ffclient "github.com/thomaspoignant/go-feature-flag"
+	"github.com/thomaspoignant/go-feature-flag/ffuser"
+	"github.com/thomaspoignant/go-feature-flag/retriever/fileretriever"
+)
+
+type user struct {
+	userId string
+	value  string
+	ffuser ffuser.User
+}
+
+var users [10000]user
+
+func main() {
+	app := fiber.New()
+
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	app.Static("/", "./public")
+
+	err := ffclient.Init(ffclient.Config{
+		PollingInterval: 500 * time.Microsecond,
+		Context:         context.Background(),
+		Retriever: &fileretriever.Retriever{
+			Path: "flags.yaml",
+		},
+	})
+
+	for i := 0; i < 10000; i++ {
+		id := i
+		u := ffuser.NewUser(fmt.Sprint(id))
+
+		users[i].ffuser = u
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		var mapToRender [10000]string
+		for i, user := range users {
+			color, _ := ffclient.StringVariation("color-flag", user.ffuser, "grey")
+			mapToRender[i] = color
+		}
+		jsonMapToRender, err := json.Marshal(mapToRender)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.SendString(string(jsonMapToRender))
+	})
+
+	app.Listen(":3030")
+}
